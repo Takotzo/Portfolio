37d8cfa359aa515510704eb3e843f3f5f0c9e3e4
Takotzo <oyvindroberg@hotmail.no>
2024-12-02 07:28:52 +0100

    BIG update :D

---
+++ main.js
@@ -0,0 +1,16 @@
+﻿let player;
+
+// This function will be called by the YouTube Iframe API when it is ready
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        videoId: '6pA61Er4rpQ?si=vo-DVlsSDyrOOha9', // Replace with your YouTube video ID
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+// This function will be called when the video is ready to play
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
