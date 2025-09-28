e3ada9ee8d6f74ca4f018d7d611ec9c9bac455ab
Takotzo <oyvindroberg@hotmail.no>
2025-09-28 17:11:09 +0200

    trying to make each accordion in a separate html file

---
+++ AccordionFiles/accordionResizer.js
@@ -0,0 +1,11 @@
+﻿
+function resizeIframe() {
+    const height = document.documentElement.scrollHeight;
+    window.parent.postMessage({ iframeHeight: height }, "*");
+}
+
+// Call it whenever content changes (e.g., after expanding text)
+document.addEventListener("DOMContentLoaded", resizeIframe);
+window.addEventListener("resize", resizeIframe);
+
+// Example: If buttons expand/collapse content dynamically, call resizeIframe() after that too
--- main.js
+++ main.js
@@ -14,3 +14,14 @@ function onYouTubeIframeAPIReady() {
 function onPlayerReady(event) {
     event.target.playVideo();
 }
+
+
+// To expand the screen space of accordion on start
+let frames = document.querySelectorAll(".accordion-iframe")
+
+frames.forEach((frame) =>{
+    frame.onload = function() {
+        frame.style.height = frame.contentWindow.document.body.scrollHeight + 15 + 'px';
+    }
+})
+
