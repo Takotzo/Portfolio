4c31b4278cc0b91ce3e215ce6af1d78fba023599
Takotzo <oyvindroberg@hotmail.no>
2025-10-08 14:30:54 +0200

    Swapped out blend shape with Rogue AI accordion

--- main.js
+++ main.js
@@ -1,22 +1,4 @@
-﻿let player;
-
-// This function will be called by the YouTube Iframe API when it is ready
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        videoId: '6pA61Er4rpQ?autoplay=1', // Replace with your YouTube video ID
-        events: {
-            'onReady': onPlayerReady
-        }
-    });
-}
-
-// This function will be called when the video is ready to play
-function onPlayerReady(event) {
-    event.target.playVideo();
-}
-
-
-// To expand the screen space of accordion on start
+﻿// To expand the screen space of accordion on start
 let frames = document.querySelectorAll(".accordion-iframe")
 
 frames.forEach((frame) =>{
